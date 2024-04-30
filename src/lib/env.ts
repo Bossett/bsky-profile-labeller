360b404a667398c4c4ce2e35188d05a8df2da6f6
Bossett <bossett@bossett.io>
2024-04-30 21:50:45 +0000

    added DANGEROUSLY_EXPOSE_SECRETS for debugging

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,12 +1,27 @@
 import { z } from 'zod'
+import logger from '@/lib/logger.js'
 
 const envSchema = z.object({
   LABELLER_HANDLE: z.string(),
   LABELLER_PASSWORD: z.string(),
   LABELLER_SERVICE: z.string().url(),
   NEON_DATABASE_URL: z.string().url(),
-  NEWHANDLE_EXPIRY: z.number().default(2592000),
+  NEWHANDLE_EXPIRY: z.coerce.number().default(2592000),
+  DANGEROUSLY_EXPOSE_SECRETS: z
+    .string()
+    .toLowerCase()
+    .transform((x) => x === 'true')
+    .pipe(z.boolean())
+    .default('false'),
 })
 
 const env = envSchema.parse(process.env)
+
+if (env.DANGEROUSLY_EXPOSE_SECRETS) {
+  logger.level = 'debug'
+  for (const key of Object.keys(env)) {
+    logger.debug(`${key}: ${env[key]}`)
+  }
+}
+
 export default env
