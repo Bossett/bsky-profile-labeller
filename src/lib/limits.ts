7c7d78273b1f881c94991060c2143ddc96c124fa
Bossett <bossett@bossett.io>
2024-05-06 05:04:36 +0000

    more limit tuning

--- src/lib/limits.ts
+++ src/lib/limits.ts
@@ -30,15 +30,15 @@ const limits = {
   // ***** APPLICATION CONFIG *****
   AUTHOR_FEED_MAX_RESULTS: 100, // sets the limit parameter requesting an author's posts - 100 is the api limit
   DB_WRITE_INTERVAL_MS: 5 * 60 * 1000, // time between pauses to update firehose sequence and scavenge cache - higher is generally betterm but you will have to reprocess this much on restart
-  MAX_CONCURRENT_PROCESSCOMMITS: 32, // this influences # of http requests, so lower can be faster
+  MAX_CONCURRENT_PROCESSCOMMITS: 48, // this influences # of http requests, so lower can be faster
   MAX_FIREHOSE_DELAY: 3 * 60 * 1000, // how long between events before considering the firehose stalled
-  MAX_PENDING_INSERTS_WAIT_MS: 3 * 60 * 1000, // the maximum amount of time between inserting pending label events
-  MAX_PENDING_INSERTS: 500, // the maximum number of label pending events before writing to the db
+  MAX_PENDING_INSERTS_WAIT_MS: 2 * 60 * 1000, // the maximum amount of time between inserting pending label events
+  MAX_PENDING_INSERTS: 100, // the maximum number of label pending events before writing to the db
   MAX_PROCESSING_TIME_MS: 20 * 1000, // the maximum time any given commit can take to process
 
   MOIZED_FETCH_MAX_AGE_MS: 30 * 1000, // how long to cache fetch results when using general fetch()
   PAUSE_TIMEOUT_MS: 3 * 60 * 1000, // how long can we pause operations waiting to write to the db
-  REGULAR_POST_STDEV_MS: 3 * 1000, // the standard deviation required for a post to be considered periodic (rapidposts)
+  REGULAR_POST_STDEV_MS: 6 * 1000, // the standard deviation required for a post to be considered periodic (rapidposts)
   USER_DETAILS_MAX_AGE_MS: 30 * 60 * 1000, // how long do cached user details live - higher is better, but can sometimes lead to stale results (cache is purged when events are emitted, so this is generally safe)
 }
 
