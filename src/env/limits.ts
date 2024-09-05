2945fa725415ea361f0a83a7c007dfe475670f4f
Bossett <bossett@bossett.io>
2024-09-05 23:50:31 +0000

    limit changes

--- src/env/limits.ts
+++ src/env/limits.ts
@@ -21,7 +21,7 @@ const limits = {
   PLC_LIMIT_MAX_RATE: undefined,
   PLC_LIMIT_RATE_INTERVAL_MS: undefined,
   // rate limit for public API
-  PUBLIC_LIMIT_MAX_CONCURRENT: undefined,
+  PUBLIC_LIMIT_MAX_CONCURRENT: 256,
   PUBLIC_LIMIT_MAX_DELAY_MS: undefined,
   PUBLIC_LIMIT_MAX_RATE: undefined,
   PUBLIC_LIMIT_RATE_INTERVAL_MS: undefined,
@@ -31,7 +31,7 @@ const limits = {
   // ***** APPLICATION CONFIG *****
   AUTHOR_FEED_MAX_RESULTS: 30, // sets the limit parameter requesting an author's posts - 30 is what bsky.app uses so the cache should be fresher
   DB_WRITE_INTERVAL_MS: 15 * 60 * 1000, // time between pauses to update firehose sequence and scavenge cache - higher is generally better but you will have to reprocess this much on restart
-  MAX_CONCURRENT_PROCESSCOMMITS: 256, // this influences # of http requests, so lower can be faster
+  MAX_CONCURRENT_PROCESSCOMMITS: 512, // this influences # of http requests, so lower can be faster
   MAX_FIREHOSE_DELAY: 3 * 60 * 1000, // how long between events before considering the firehose stalled
   MIN_FIREHOSE_OPS: 30, // the minimum number of operations per interval before considering the firehose stalled
   MAX_PENDING_INSERTS_WAIT_MS: 30 * 1000, // the maximum amount of time between inserting pending label events
@@ -39,13 +39,13 @@ const limits = {
   MAX_PROCESSING_TIME_MS: 2 * 60 * 1000, // the maximum time any given commit can take to process
   REGULAR_POST_STDEV_MS: 12 * 1000, // the standard deviation required for a post to be considered periodic (rapidposts)
   USER_DETAILS_MAX_AGE_MS: 3 * 60 * 60 * 1000, // how long do cached user details live - higher is better, but can sometimes lead to stale results (cache is purged when events are emitted, so this is generally safe)
-  USER_DETAILS_MAX_SIZE: 50000,
+  USER_DETAILS_MAX_SIZE: 25000,
   AUTHOR_FEED_MAX_AGE_MS: 3 * 60 * 60 * 1000, // as above for author feed, resets on post
-  AUTHOR_FEED_MAX_SIZE: 50000,
+  AUTHOR_FEED_MAX_SIZE: 25000,
   PLC_DIRECTORY_MAX_AGE_MS: 3 * 60 * 60 * 1000,
-  PLC_DIRECTORY_MAX_SIZE: 50000,
+  PLC_DIRECTORY_MAX_SIZE: 25000,
   POST_CACHE_MAX_AGE_MS: 3 * 60 * 60 * 1000,
-  POST_CACHE_MAX_SIZE: 500,
+  POST_CACHE_MAX_SIZE: 1500,
   MIN_BATCH_WAIT_TIME_MS: 100,
   BATCH_CYCLE_TIMEOUT_MS: 3 * 60 * 1000,
 }
