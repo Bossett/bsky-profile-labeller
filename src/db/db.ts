aafe9612147892b57b691edccfa560b66e3ab253
Bossett <bossett@bossett.io>
2024-09-06 03:23:09 +0000

    pooled connections, tweaked limit

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,6 @@
 import env from '@/env/env.js'
 import * as schema from '@/db/schema.js'
 
-/*
 import { Pool, neonConfig } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-serverless'
 
@@ -10,13 +9,15 @@ import ws from 'ws'
 neonConfig.webSocketConstructor = ws
 
 const pool = new Pool({ connectionString: env.NEON_DATABASE_URL })
-const db = drizzle(pool, { schema: schema })*/
+const db = drizzle(pool, { schema: schema })
 
+/*
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const queryClient = postgres(env.NEON_DATABASE_URL)
 const db = drizzle(queryClient, { schema: schema })
+*/
 
 export default db
 
