f7c55b03eeba551f41ba6bd8a8b7c544f45b906b
Bossett <bossett@bossett.io>
2024-09-07 01:16:11 +0000

    fix POSTGRES_DATABASE_URL

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import env from '@/env/env.js'
 import postgres from 'postgres'
-const connectionString = env.NEON_DATABASE_URL
+const connectionString = env.POSTGRES_DATABASE_URL
 const sql = postgres(connectionString, { max: 1 })
 const db = drizzle(sql)
 await migrate(db, { migrationsFolder: 'drizzle' })
